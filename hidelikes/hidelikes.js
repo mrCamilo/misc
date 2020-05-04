e4789b86f7c3ed2c3bc24b939472753584f070b3
Manuel <mcamilo@protonmail.com>
2020-05-04 10:39:16 -0400

    hide all youtube elements but video title and home button

--- hidelikes/hidelikes.js
+++ hidelikes/hidelikes.js
@@ -1,2 +1,6 @@
-// This line hides the container on youtube.
-document.getElementById("menu-container").style.display = "none";
+// remove various youtube elements
+// Combine with "Distraction Free Youtube" extension for a youtube that is literally just a home button, a search bar, and video content.
+document.getElementById("menu-container").style.display = "none"; // Hides youtube container that displays likes and dislikes
+document.getElementById("end").style.display = "none"; // hides login button 
+document.getElementById("subscribe-button").style.display = "none";// hides subscribe button, description
+document.getElementById("meta").style.display = "none";
