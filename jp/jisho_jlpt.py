fd77b8d59e7465942dc17af066b029f124e0c37d
mrCamilo <mcamilo@protonmail.com>
2020-05-22 06:02:35 -0400

    returns JLPT level

--- jp/jisho_jlpt.py
+++ jp/jisho_jlpt.py
@@ -10,6 +10,8 @@ url = 'https://jisho.org/api/v1/search/words?keyword=' + word
 r = requests.get(url)
 response = r.json() #store response in a variable
 dic_search = response['data']
+my_dict = dic_search[0] #just take jlpt of 1st word
+jlpt_dict = my_dict['jlpt']
 
-print(dic_search)
+print(f"JLPT Level: {jlpt_dict}")
 
