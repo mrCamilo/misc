c4a17c7a46071fff2b5eceb843d755a2cb20e286
mrCamilo <mcamilo@protonmail.com>
2020-05-22 18:42:07 -0400

    try&except so its harder to break it

--- jp/jisho_jlpt.py
+++ jp/jisho_jlpt.py
@@ -9,18 +9,20 @@ while active:
     if word == 'q':
         active = False
     else:
-        #Make API call
-        url = 'https://jisho.org/api/v1/search/words?keyword=' + word
-        r = requests.get(url)
-        response = r.json() #store response in a variable
-        dic_search = response['data']
-        my_dict = dic_search[0] #just take 1st word
-        jlpt_dict = my_dict['jlpt'] #filters out other data for JLPT level
+        try:
+            #Make API Call
+            url = 'https://jisho.org/api/v1/search/words?keyword=' + word
+            r = requests.get(url)
+            response = r.json() #store response in a variable
+            dic_search = response['data']
+            my_dict = dic_search[0] #just take 1st word
+            jlpt_dict = my_dict['jlpt'] #filters out other data for JLPT level
 
-        #Convert list to string using join()
-        word = ''.join(jlpt_dict)
-
-        pretty_word = word.replace("jlpt-n", "N")
-        print(f"It's an {pretty_word} word.")
+            #Convert list to string using join(), make look nice
+            word = ''.join(jlpt_dict)
+            word = word.replace("jlpt-n", "N")
+            print(f"It's an {word} word.")
+        except:
+            print("Error. Try again, or press 'q' to quit.")
 
 print("Goodbye.")
