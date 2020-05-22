97ad52f81bae208a8a2662c49a8adf4633aa9522
mrCamilo <mcamilo@protonmail.com>
2020-05-22 17:38:50 -0400

    format the result

--- jp/jisho_jlpt.py
+++ jp/jisho_jlpt.py
@@ -1,5 +1,14 @@
-###Use jisho to look up what jlpt level a particular word is###
+#Use jisho to look up what jlpt level a particular word is
 import requests
+
+#prettify the API result 
+def prettify(a_list):
+    a_list[a_list.index('jlpt-n5')] = 'N5'
+    a_list[a_list.index('jlpt-n4')] = 'N4'
+    a_list[a_list.index('jlpt-n3')] = 'N3'
+    a_list[a_list.index('jlpt-n2')] = 'N2'
+    a_list[a_list.index('jlpt-n1')] = 'N1'
+    return a_list
 
 print('Enter \'q\' to quit')
 
@@ -16,6 +25,10 @@ while active:
         response = r.json() #store response in a variable
         dic_search = response['data']
         my_dict = dic_search[0] #just take 1st word
-        jlpt_dict = my_dict['jlpt'] #filters out 1st word's JLPT level
+        jlpt_dict = my_dict['jlpt'] #filters out other data for JLPT level
 
-        print(jlpt_dict)
+        # prettify API result and print
+        prettify(jlpt_dict)
+        print(f"It's an {jlpt_dict} word.")
+
+print("Goodbye.")
