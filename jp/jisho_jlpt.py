f0d9b8db04c52b9e0359c14f9e8d830208c20d1b
mrCamilo <mcamilo@protonmail.com>
2020-05-22 05:48:20 -0400

    dictionary lookups

--- jp/jisho_jlpt.py
+++ jp/jisho_jlpt.py
@@ -7,9 +7,9 @@ word = input("Which word's JLPT level do you want to look up?")
 #Make API call to Jisho
 url = 'https://jisho.org/api/v1/search/words?keyword=' + word
 
-r=requests.get(url)
+r = requests.get(url)
+response = r.json() #store response in a variable
+dic_search = response['data']
 
-response = r.json()
-
-print(response)
+print(dic_search)
 
