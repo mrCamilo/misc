d736eabc4762464adab63996a792c7dfed93759b
mrCamilo <mcamilo@protonmail.com>
2020-06-17 12:27:13 -0400

    all the questions for the form

---
+++ passaic_peds_forms/symptom_checklist/src/questionsData.js
@@ -0,0 +1,13 @@
+const questionsData = [
+    {
+        id: 1,
+	question: "Complain of aches or pains"
+    },
+    {
+	id: 2,
+	question: "Spend more time alone"
+    }
+]
+
+
+export default questionsData
