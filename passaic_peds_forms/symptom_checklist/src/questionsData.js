69c13d5888b985b11794b680759311b05ebc90a1
mrCamilo <mcamilo@protonmail.com>
2020-06-17 14:20:14 -0400

    all the psc-y questions separately

--- passaic_peds_forms/symptom_checklist/src/questionsData.js
+++ passaic_peds_forms/symptom_checklist/src/questionsData.js
@@ -1,11 +1,155 @@
 const questionsData = [
     {
         id: 1,
-	question: "Complain of aches or pains"
+	question: "Complain of aches or pains",
+	category: ""
     },
     {
 	id: 2,
-	question: "Spend more time alone"
+	question: "Spend more time alone",
+	category: ""
+    },
+    {
+	id: 3,
+	question: "Tire easily, little energy",
+	category: ""
+    },
+    {
+	id: 4,
+	question: "Fidgety, unable to sit still",
+        category: ""
+    },
+    {
+	id: 5,
+	question: "Have trouble with teacher",
+
+    },
+    {
+	id: 6,
+	question: "Less interested in school"
+    },
+    {
+	id: 7,
+	question: "Less interested in school"
+    },
+    {
+	id: 8,
+	question: "Act as if driven by motor"
+    },
+    {
+	id: 9,
+	question: "Daydream too much"
+    },
+    {
+	id: 10,
+	question: "Distract easily"
+    },
+    {
+	id: 11,
+	question: "Are afraid of new situations"
+    },
+    {   
+	id: 12,
+	question: "Feel sad, unhappy"
+    },
+    {   id: 13,
+	question: "Are irritable, angry"
+    },
+    {
+	id: 14,
+	question: "Feel hopeless"
+    },
+    {
+        id: 15,
+        question: "Have trouble concentrating"
+    },
+    {
+	id: 16,
+	question: "Less interested in friends"
+    },
+    {   
+	id: 17,
+	question: "Fight with other children"
+    },
+    {
+	id: 18,
+	question: "Absent from school"
+    },
+    {
+	id: 19,
+	question: "School grades dropping"
+    },
+    {
+	id: 20,
+	question: "Down on yourself"
+    },
+    {
+	id: 21,
+	question: "Visit doctor with doctor finding nothing wrong"
+    },
+    {
+	id: 22,
+	question: "Have trouble sleeping"
+    },
+    {
+	id: 23,
+	question: "Worry a lot"
+    },
+    {
+	id: 24,
+	question: "Want to be with parent more than before"
+    },
+    {
+	id: 25,
+	question: "Feel that you are bad"
+    },
+    {
+	id: 26,
+	question: "Take unnecessary risks"
+    },
+    {
+	id: 27,
+	question: "Get hurt frequently"
+    },
+    {
+	id: 28,
+	question: "Seem to be having less fun"
+    },
+    {
+	id: 29,
+	question: "Act younger than children your age"
+    },
+    {
+	id: 30,
+        question: "Do not listen to rules"
+    },
+    {
+	id: 31,
+	question: "Do not show feelings"
+    },
+    {
+	id: 32,
+	question: "Do not understand other people's feelings"
+    },
+    {
+	id: 33,
+	question: "Tease others"
+    },
+    {	
+	id: 34,
+	question: "Blame others for your troubles"
+    },
+    {
+	id: 35,
+	question: "Take things that do not belong to you"
+    },
+    {
+	id: 36,
+	question: "Refuse to share"
+    },
+    {
+	id: 37,
+	question: "Do you have any emotional or behavioral problems for which you need help?"
     }
 ]
 
