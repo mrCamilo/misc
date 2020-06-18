15c5a022e623bf64f68a19a379b48aa80740191b
mrCamilo <mcamilo@protonmail.com>
2020-06-18 15:45:06 -0400

    refactor

---
+++ passaic_peds_forms/symptom_checklist/src/components/MapQuestions.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import Header from './Header.js'
+import Questionnaire from './Questionnaire.js'
+import questionsData from '../questionsData.js'
+import Buttons from './Buttons'
+import Results from './Results'
+
+function mapQuestions() {
+    const questComponents = questionsData.map(x => {
+        return (
+            <div>
+	        <Questionnaire question = {x.question} />
+		<Buttons/>
+	    </div>
+	)
+    })
+
+    return (
+        <form>
+            <Header/>
+            {questComponents}
+	    <Results/>
+	</form>
+    )
+}
+
+export default mapQuestions 
