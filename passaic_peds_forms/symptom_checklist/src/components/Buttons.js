2e8cb850b5157ccd2439d12314a11c078018ca7c
mrCamilo <mcamilo@protonmail.com>
2020-06-18 15:31:35 -0400

    Simplified buttons

--- passaic_peds_forms/symptom_checklist/src/components/Buttons.js
+++ passaic_peds_forms/symptom_checklist/src/components/Buttons.js
@@ -1,37 +1,19 @@
 import React from 'react'
 
-class Buttons extends React.Component {
-    constructor(props) {
-	    super(props)
-	    this.state = {
-		selectedValue: 0,
-		totalScore: 0
-	    }
-    
-	    this.handleChange = this.handleChange.bind(this)
-    }
-
-    //This updates state!
-    handleChange(event) {
-	this.setState({
-		selectedValue: event.target.value
-	})
-    }
-
-    render() {
-        return (
-	    <form>
-	        <input type = "radio" name="group" value = {0} onChange={this.handleChange}/>
-	        <label for = "never">Never</label>
-	        <br/>
-	        <input type = "radio" name="group" value = {1} onChange={this.handleChange}/>
-	        <label for = "sometimes">Sometimes</label>
-	        <br/>
-	        <input type = "radio" name="group" value = {2} onChange={this.handleChange}/>
-	        <label for = "always">Always</label>
-	    </form>
-        )
-    }  
-} 
+function Buttons() {
+    return(
+	<form>
+	    <input type = "radio" value = "Never" name="group"/>
+	    <label>Never</label>
+	    <br/>
+	    <input type = "radio" value = "Sometimes" name="group"/>
+	    <label>Sometimes</label>
+	    <br/>
+	    <input type = "radio" value = "Always" name="group"/>
+	    <label>Always</label>
+	    <br/>
+	</form>
+    )
+}
 
 export default Buttons
