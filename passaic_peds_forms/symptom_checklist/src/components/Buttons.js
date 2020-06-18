73ea689014908ac3b873d8fd0c92d996875ffd50
mrCamilo <mcamilo@protonmail.com>
2020-06-18 10:33:47 -0400

    add state to all checkboxes to keep track of total

--- passaic_peds_forms/symptom_checklist/src/components/Buttons.js
+++ passaic_peds_forms/symptom_checklist/src/components/Buttons.js
@@ -1,20 +1,39 @@
 import React from 'react'
 
-function Buttons() {
-   return (
-	<div>
-	   <form>
-	       <input type = "radio" name="group" id = "never"/>
-	       <label for = "never">Never</label>
-	       <br/>
-	       <input type = "radio" name="group" id = "sometimes"/>
-	       <label for = "sometimes">Sometimes</label>
-	       <br/>
-	       <input type = "radio" name="group" id = "always"/>
-	       <label for = "always">Always</label>
-	   </form>
-	</div>
-   )
-}
+class Buttons extends React.Component {
+    constructor(props) {
+	    super(props)
+	    this.state = {
+		selectedValue: 0 
+	    }
+    
+	    this.handleChange = this.handleChange.bind(this)
+    }
+
+    //This updates state!
+    handleChange(event) {
+	this.setState({
+		selectedValue: event.target.value
+	})
+    }
+
+    render() {
+        return (
+		<div>
+	    <form>
+	        <input type = "radio" name="group" value = {0} onChange={this.handleChange}/>
+	        <label for = "never">Never</label>
+	        <br/>
+	        <input type = "radio" name="group" value = {1} onChange={this.handleChange}/>
+	        <label for = "sometimes">Sometimes</label>
+	        <br/>
+	        <input type = "radio" name="group" value = {2} onChange={this.handleChange}/>
+	        <label for = "always">Always</label>
+	    </form>
+		<p>{this.state.selectedValue}</p>
+		</div>
+        )
+    }  
+} 
 
 export default Buttons
