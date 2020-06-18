1e2ad40a39b512069c9215192cc62f1a98ebd901
mrCamilo <mcamilo@protonmail.com>
2020-06-18 10:34:22 -0400

    need to make formula for 4 different scores

--- passaic_peds_forms/symptom_checklist/src/components/Results.js
+++ passaic_peds_forms/symptom_checklist/src/components/Results.js
@@ -1,26 +1,15 @@
 import React, {Component} from 'react'
+import selectedValue from './Buttons' 
 
-class Results extends Component {
-    constructor() {
-	super()
-	this.state = {
-		score: 0,
-		anxiety: 0,
-		attention: 0,
-		conduct: 0
-	} 
-    }
-
-    render() {
+function Results () {
         return(
             <div>
-	        <p>Score: {this.state.score}</p>
-	        <p>Anxiety/Depression Score: {this.state.anxiety}</p>
-	        <p>Attention Problem Score: {this.state.attention}</p>
-	        <p>Conduct Problem Score: {this.state.conduct}</p>
+	        <p>Score: {selectedValue}</p>
+	        <p>Anxiety/Depression:  </p>
+	        <p>Externalizing: </p>
+	        <p>Internalizing: </p>
 	    </div>
         )
     }
-}	
 
 export default Results
